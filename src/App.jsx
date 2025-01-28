5924e23f52b63d4fca799c60a4dbebac7f67d17d
khalilaliev <khalilaliev0@gmail.com>
2025-01-28 22:13:30 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="container mx-auto ">
+        <h1 className="text-zinc font-bold">Hello world!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
